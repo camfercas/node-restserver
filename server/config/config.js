89aca1204ff3459da095c398f8ff79f1dfcbe05d
Camilo <camfercas@gmail.com>
2020-08-08 14:48:34 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb+srv://camfercas:hrNUBkhGeY0x9equ@cluster0.2tbf1.mongodb.net/cafe';
+    urlBD = process.env.MONGO_URI;
 }
 
 process.env.urlBD = urlBD;
